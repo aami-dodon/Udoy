d26070364cbd804fa057d9bedc2c3fedd5aafa85
Sayantan Basu <62693630+sayabasu@users.noreply.github.com>
2025-10-20 02:10:30 +0530

    feat: add theme showcase route

--- app/client/src/routes.jsx
+++ app/client/src/routes.jsx
@@ -1,9 +1,14 @@
 import HomePage from './features/home/HomePage.jsx';
+import ThemeShowcasePage from './features/theme/ThemeShowcasePage.jsx';
 
 const routes = [
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/theme',
+    element: <ThemeShowcasePage />,
   },
 ];
 
